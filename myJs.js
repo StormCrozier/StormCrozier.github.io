0e736592a191ce2c22ae9ed55ab68918b7fd4525
StormCrozier <stormcrozier950@gmail.com>
2022-01-05 19:08:12 -0800

    Update to contact form

--- myJs.js
+++ myJs.js
@@ -1,3 +1,4 @@
+var fields = {};
 $(document).ready(function () {
     const addToDom = () =>{
     $('#navbar').append(
@@ -13,3 +14,63 @@ $(document).ready(function () {
     }
     addToDom();
 });
+
+function getDomEle() {
+    fields.name = document.getElementById('name');
+    fields.email = document.getElementById('email');
+    fields.message = document.getElementById('message');
+}
+
+function isNotEmpty(value) {
+    if(value == null || typeof value == 'undefined') 
+        return false;
+    return(value.length > 0);
+}
+
+function isEmail(email) {
+    let regex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
+
+    return regex.test(String(email).toLowerCase());
+}
+
+function isValid(fields) {
+    var nameCheck;
+    var emailCheck1;
+    var emailCheck2;
+    var messageCheck;
+
+    nameCheck = isNotEmpty(fields.name.value);
+    emailCheck1 = isNotEmpty(fields.email.value);
+    emailCheck2 = isEmail(fields.email.value);
+    messageCheck = isNotEmpty(fields.message.value);
+
+    if(nameCheck, emailCheck1, emailCheck2, messageCheck)
+        return true;
+    else 
+        return false;
+}
+
+function sendContact(){
+    getDomEle();
+    if(isValid(fields)){
+        alert('$(fields.name.value} thanks for contacting')
+        console.log(fields);
+    }
+    else{
+        alert('An error occured');
+    }
+}
+
+function sendMail() {
+    var link = "mailto:stormcrozier950@gmail.com"
+             + "?cc="
+             + "&subject=" + encodeURIComponent("Message From Website")
+             + "&body=" + encodeURIComponent(document.getElementById('name').value)
+             + encodeURIComponent("\n")
+             + encodeURIComponent(document.getElementById('email').value)
+             + encodeURIComponent("\n")
+             + encodeURIComponent(document.getElementById('message').value);
+    
+    
+    window.location.href = link;
+}
